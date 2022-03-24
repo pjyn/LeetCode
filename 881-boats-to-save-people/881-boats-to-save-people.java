1a1ecf2395b972ebe0b398ce8962242f966113cb
pjyn <69898451+pjyn@users.noreply.github.com>
2022-03-24 22:07:48 +0530

    Time: 20 ms (61.45%), Space: 65.1 MB (60.12%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+     
+        Arrays.sort(people);
+        
+        int numBoats = 0;
+        int i=0; 
+        int j=people.length-1;
+        
+        // [1, 2, 2, 3]
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+            }else{
+                j--;
+            }
+            numBoats++;
+        }
+        return numBoats;
+    }
+}
