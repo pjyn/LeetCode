12e9e26921b5d6b13fe3482efd13025978f4b8db
pjyn <69898451+pjyn@users.noreply.github.com>
2022-04-01 16:36:28 +0530

    Time: 1 ms (84.55%), Space: 58.6 MB (5.10%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -6,14 +6,24 @@ class Solution {
             
             [o, l, l, e, h]
         */
-        String str = String.valueOf(s); // "hello"
-        StringBuilder sb = new StringBuilder();
-        sb.append(str);
+//         String str = String.valueOf(s); // "hello"
+//         StringBuilder sb = new StringBuilder();
+//         sb.append(str);
         
-        str = sb.reverse().toString();   // "olleh"
-        int cnt = 0;
-        for(char c: str.toCharArray()){
-            s[cnt++] = c;
+//         str = sb.reverse().toString();   // "olleh"
+//         int cnt = 0;
+//         for(char c: str.toCharArray()){
+//             s[cnt++] = c;
+//         }
+        int starting = 0;
+        int ending = s.length-1;
+        
+        while(starting < ending){
+            char tmp = s[starting];
+            s[starting] = s[ending];
+            s[ending] = tmp;
+            starting++;
+            ending--;
         }
     }
 }
