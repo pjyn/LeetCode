27995d70f8452da73e5c06fdc421f9e178034815
pjyn <69898451+pjyn@users.noreply.github.com>
2022-04-12 18:31:11 +0530

    Time: 1 ms (55.24%), Space: 41.9 MB (76.21%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.java
@@ -0,0 +1,69 @@
+class Solution {
+    // private int[][] dir = new int[][]{{-1, -1}, {-1, 0}, {-1, +1}, {0, -1}, {0, +1}, {+1, -1}, {+1, 0}, {+1, +1}};    
+    
+    private int[][] dir = new int[][]{{0,1}, {1,0}, {-1, 0}, {0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+    
+    public void gameOfLife(int[][] board) {
+        
+        // 3-> lives
+        // -2 -> dies
+        
+        int row = board.length;
+        int col = board[0].length;
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                
+                if(board[i][j] == 1){
+                    int tmp = helper(i, j, board, row, col);
+                    if(tmp < 2 || tmp > 3){
+                        board[i][j] = -2;
+                    }
+                    // else if(tmp == 2 || tmp == 3){
+                    //     board[i][j] = 3;
+                    // }
+                }else if(board[i][j] == 0){
+                    int tmp = helper(i, j, board, row, col);
+                    if(tmp == 3){
+                        board[i][j] = 3;
+                    }
+                }
+            }
+        }
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                
+                if(board[i][j] == 3){
+                    board[i][j] = 1;
+                }
+                if(board[i][j] == -2){
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    private int helper(int i, int j, int[][] board, int row, int col){
+        
+        int active = 0;
+//         if(i < 0 || j < 0 || i >= row || j >= col || board[i][j] == 0){
+//             return 0;
+//         }
+        
+//         if(board[i][j] == 1){
+//             active++;
+//         }
+        for(int[] direction: dir){
+            
+            int newRow = i+direction[0];
+            int newCol = j+direction[1];
+            
+            if(newRow >= 0 && newRow < row && newCol >= 0 && newCol < col && (board[newRow][newCol] == 1 || board[newRow][newCol] == -2)){
+                active++;
+            }
+        }
+        
+        return active;
+    }
+}
