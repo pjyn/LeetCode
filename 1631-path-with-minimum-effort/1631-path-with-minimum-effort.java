7339ab73414322bc0183648adb037327e846984b
pjyn <69898451+pjyn@users.noreply.github.com>
2022-04-28 23:22:43 +0530

    Time: 47 ms (92.75%), Space: 43.3 MB (77.78%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,8 +1,7 @@
 class Solution {
-    // TC: O(n*m*log(n*m))
-    // SC: O(m*n)
-    int[] dir = new int[]{ 0, 1, 0,-1, 0};
+    int[] dir = new int[]{0, 1, 0, -1, 0};
     public int minimumEffortPath(int[][] heights) {
+        
         int m = heights.length;
         int n = heights[0].length;
         int[][] efforts = new int[m][n];
@@ -10,7 +9,7 @@ class Solution {
             Arrays.fill(effort, Integer.MAX_VALUE);
         }
         
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> (a[0]-b[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> (a[0]-b[0]));
         efforts[0][0] = 0;
         
         pq.offer(new int[]{0,0,0});
@@ -18,29 +17,28 @@ class Solution {
         while(!pq.isEmpty()){
             int[] head = pq.poll();
             int x = head[1];
-            int y =head[2];
+            int y = head[2];
             int currentEffort = head[0];
-            if(x == m-1 && y == n-1){
+            if(x == m-1 && y == n-1){   // terminal node
                 return currentEffort;
             }
-            for(int k=0;k<4;k++){
+            for(int k=0; k<4; k++){
                 int newX = x + dir[k];
                 int newY = y + dir[k+1];
-                //k= 0 x, y+1
-                //k=1 x+1, y 
-                // k=2 x, y-1
-                // k-3 x-1, y
-                if(newX>=0 && newX<m && newY>=0 &&newY<n){
-                    int nextEffort = Math.max(currentEffort, Math.abs(heights[newX][newY] - heights[x][y]));
+                // k=0 x, y+1
+                // k=1 x+1, y
+                // k=2 x-1, y
+                // k=3 x, y-1
+                if(newX >= 0 && newX < m && newY >= 0 && newY < n){
+                    int nextEffort = Math.max(currentEffort, Math.abs(heights[newX][newY]-heights[x][y]));
                     
-                    if(nextEffort< efforts[newX][newY]){
-                        efforts[newX][newY] =nextEffort;
+                    if(nextEffort < efforts[newX][newY]){
+                        efforts[newX][newY] = nextEffort;
                         pq.offer(new int[]{nextEffort, newX, newY});
                     }
                 }
             }
         }
         return 0;
-            
     }
 }
