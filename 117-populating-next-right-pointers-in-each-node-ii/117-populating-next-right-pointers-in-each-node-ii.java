bbbab4361a7a638ca18f5e11027eff7613eadc2c
pjyn <69898451+pjyn@users.noreply.github.com>
2022-05-13 22:28:50 +0530

    Time: 4 ms (8.31%), Space: 45.3 MB (23.09%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -24,29 +24,31 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         
-        if (root==null){
+        if(root == null){
             return null;
         }
         
         Queue<Node> q = new LinkedList<>();
         q.add(root);
         q.add(null);
-        while (!q.isEmpty()){
+        
+        while(!q.isEmpty()){
             Node curr = q.poll();
-            if (curr == null && q.isEmpty()){
+            if(curr == null && q.isEmpty()){
                 return root;
-            }else if(curr == null){
+            }
+            else if(curr == null){
                 q.add(null);
                 continue;
             }else{
                 curr.next = q.peek();
-                if(curr.left!=null){
+                if(curr.left != null){
                     q.add(curr.left);
+                    
                 }
-                if(curr.right!=null){
+                if(curr.right != null){
                     q.add(curr.right);
                 }
-                
             }
         }
         return root;
