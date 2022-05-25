52a7fdb2a86d971244b2470f94410db2c201ae61
pjyn <69898451+pjyn@users.noreply.github.com>
2022-05-25 16:29:53 +0530

    Time: 66 ms (62.80%), Space: 79.6 MB (77.34%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,28 +1,26 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
+     
+        Arrays.sort(envelopes, (a,b)-> (a[0]==b[0]) ? (b[1]-a[1]) : (a[0]-b[0]));
+        int n = envelopes.length;
         
-        Arrays.sort(envelopes, (a, b)-> (a[0]==b[0]) ? (b[1]-a[1]) : (a[0]-b[0]));
-                
-        int[] dp = new int[envelopes.length];
+        int[] dp = new int[n];
+        int maxlen = 0;
         
-        int max = 0;
-        
-        for(int i=0; i<dp.length; i++){
+        for(int i=0; i<n; i++){
             
-            // find index of height of envelope[i]
-            int index = binarySearch(dp, 0, max, envelopes[i][1]);
-            
+            int index = binarySearch(dp, 0, maxlen, envelopes[i][1]);
             dp[index] = envelopes[i][1];
             
-            if(index == max){
-                max++;
+            if(maxlen == index){
+                maxlen++;
             }
         }
-        return max;
+        return maxlen;
     }
-    
-    int binarySearch(int[] arr, int start, int end, int target){
-        int index = Arrays.binarySearch(arr, start, end, target);
+    private int binarySearch(int[] dp, int start, int end, int target){
+        
+        int index = Arrays.binarySearch(dp, start, end, target);
         if(index < 0){
             index = -(index+1);
         }
