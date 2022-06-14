dbb6249c4c80bee6fe2f2bb3b86b0adea7a41930
pjyn <69898451+pjyn@users.noreply.github.com>
2022-06-14 17:35:09 +0530

    Added solution - LeetHub

---
+++ Look and Say Pattern - GFG/look-and-say-pattern.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            int n = Integer.parseInt(read.readLine());
+            
+            
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.lookandsay(n));
+        }
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String lookandsay(int n) {
+        
+        if(n == 1)
+            return "1";
+            
+        StringBuilder sb = new StringBuilder();
+        String s = lookandsay(n-1) + "0";     // 5, 4, 3, 2, 1
+        
+        for(int i=0, count=1; i<s.length()-1; i++, count++){
+            if(s.charAt(i+1) != s.charAt(i)){
+                sb.append(count).append(s.charAt(i));
+                count = 0;
+            }
+        }
+        return sb.toString();
+        
+        /*
+            static String lookandsay(int n) {
+        if(n == 1) return "1";
+        StringBuilder res = new StringBuilder();
+        String s = lookandsay(n-1) + "0"; 
+        for(int i=0, count=1; i < s.length()-1; i++, count++){
+            if(s.charAt(i+1) != s.charAt(i)){
+                res.append(count).append(s.charAt(i));
+                count=0;
+            }
+        }
+    return res.toString();      
+   }
+        */
+    }
+}
