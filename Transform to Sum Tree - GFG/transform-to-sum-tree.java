492be011648034b2b586e2ccf15235e922c392fa
pjyn <69898451+pjyn@users.noreply.github.com>
2022-06-22 22:03:41 +0530

    Added solution - LeetHub

--- Transform to Sum Tree - GFG/transform-to-sum-tree.java
+++ Transform to Sum Tree - GFG/transform-to-sum-tree.java
@@ -110,6 +110,19 @@ class GfG {
 
 //User function template for JAVA
 
+/*Complete the Given Function
+
+Node is as follows:
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+    	data=d;
+    	left=null;
+    	right=null;
+    }
+}*/
+
 class Solution{
     public void toSumTree(Node root){
         solve(root);
@@ -122,7 +135,6 @@ class Solution{
         int b = solve(curr.right);
         int x = curr.data;
         curr.data = a+b;
-        
         return a+b+x;
     }
 }
