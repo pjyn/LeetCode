c83c0148baf3207f557318d4554caca5f7c9bdf8
pjyn <69898451+pjyn@users.noreply.github.com>
2022-06-23 16:10:25 +0530

    Time: 57 ms (55.00%), Space: 75.3 MB (10.00%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        
         Arrays.sort(courses, (a, b)-> (a[1]-b[1]));
         
-        int timeElapsed = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>( (a, b) -> (b-a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b-a));
         
+        int timeElapsed = 0;
         for(int[] course: courses){
             if(timeElapsed + course[0] <= course[1]){
                 pq.offer(course[0]);
                 timeElapsed += course[0];
             }else if(!pq.isEmpty() && pq.peek() > course[0]){
-                timeElapsed -= pq.remove();
+                timeElapsed -= pq.poll();
                 pq.offer(course[0]);
                 timeElapsed += course[0];
             }else{
