c4f83f5cc439de31e6b51c2802063723f4eeb09b
pjyn <69898451+pjyn@users.noreply.github.com>
2022-07-06 15:33:51 +0530

    Time: 3 ms (44.58%), Space: 40.7 MB (79.67%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+     
+        HashSet<Integer> hs = new HashSet<>();
+        
+        while(n != 1){
+            int current = n;
+            int sum = 0;
+            while(current != 0){
+                int tmp = current%10; // 
+                sum += (tmp*tmp);
+                current /= 10;
+            }
+            if(hs.contains(sum))
+                return false;
+            
+            hs.add(sum);
+            n = sum;
+        }
+        return true;
+    }
+}
