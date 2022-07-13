1930c1b8df690d1d8c011cc7d3238511a98a0ce4
pjyn <69898451+pjyn@users.noreply.github.com>
2022-07-13 23:15:30 +0530

    Time: 153 ms (56.94%), Space: 40.3 MB (89.75%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -6,7 +6,6 @@ class Solution {
         
         int perimeter = IntStream.of(matchsticks).sum();
         
-        
         if(perimeter % 4 != 0)
             return false;
         
@@ -14,32 +13,53 @@ class Solution {
         int[] sides = new int[]{side, side, side, side};
         
         Arrays.sort(matchsticks);
-        return helper(matchsticks, matchsticks.length-1, sides);
+        reverse(matchsticks);
+        return helper(matchsticks, 0, sides);
+    }
+    private void reverse(int[] matchsticks){
+        int i=0;
+        int j=matchsticks.length-1;
+        
+        while(i <= j){
+            int tmp = matchsticks[i];
+            matchsticks[i] = matchsticks[j];
+            matchsticks[j] = tmp;
+            i++;
+            j--;
+        }
     }
     
     private boolean helper(int[] nums, int idx, int[] sides){
         
-        if(idx == -1)
-            return allZero(sides);
-        
-        
-        for(int j=0; j<sides.length; j++){
-            if(nums[idx] <= sides[j]){
-                
-                sides[j] -= nums[idx];
-                if(helper(nums, idx-1, sides))
-                    return true;
-                sides[j] += nums[idx];    
+        // if(idx == -1)
+        //     return allZero(sides);
+
+        if(idx == nums.length){
+            if(sides[0] == 0 && sides[1] == 0 && sides[2] == 0 && sides[3] == 0){
+                return true;
+            }else{
+                return false;
             }
-            
         }
+        
+        for(int j=0; j<4; j++){
+            if(nums[idx] > sides[j])
+                continue;
+            sides[j] -= nums[idx];
+            
+            if(helper(nums, idx+1, sides))
+                return true;
+            sides[j] += nums[idx];    
+        }
+            
+        // }
         return false;
     }
-    private boolean allZero(int[] sides){
-        for(int i=0; i<sides.length; i++){
-            if(sides[i] > 0)
-                return false;
-        }
-        return true;
-    }
+    // private boolean allZero(int[] sides){
+    //     for(int i=0; i<sides.length; i++){
+    //         if(sides[i] > 0)
+    //             return false;
+    //     }
+    //     return true;
+    // }
 }
