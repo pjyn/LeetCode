f34eb5839d42723e0948cb056987a3aeca8d2e2c
pjyn <69898451+pjyn@users.noreply.github.com>
2022-07-22 15:26:50 +0530

    Time: 84 ms (8.78%), Space: 79.2 MB (6.99%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,36 +1,52 @@
 class Solution {
+    
+    private class Point{
+        private int x;
+        private int y;
+        
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
         
-        if(grid[0][0] == 1)
+        int n = grid.length - 1;
+        if(grid[0][0] == 1 || grid[n][n] == 1){
             return -1;
-        int m = grid.length;
-        int n = grid[0].length;
+        }
+        int level = 0;
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(0, 0));
         
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 1});
-        grid[0][0] = 1;
-        
-        int[][] dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
         while(!q.isEmpty()){
             int size = q.size();
             while(size-- > 0){
-                int[] point = q.poll();
+                Point head = q.poll();
+                int x = head.x;
+                int y = head.y;
                 
-                if(point[0] == m-1 && point[1] == n-1)      // breaking statement.
-                    return point[2];
-                
-                for(int[] d: dir){
-                    int r = point[0] + d[0];
-                    int c = point[1] + d[1];
-                    
-                    if(r >= 0 && c >= 0 && r < m && c < n && grid[r][c] == 0){
-                        q.add(new int[]{r, c, point[2]+1});
-                        grid[r][c] = 1;
-                    }
+                if(x == n && y == n){
+                    return level+1;
                 }
+                if(x < 0 || y < 0 || x>n || y>n || grid[x][y] >= 1)
+                    continue;
+                
+                grid[x][y] = 2;
+                q.offer(new Point(x-1, y));
+                q.offer(new Point(x+1, y));
+                q.offer(new Point(x, y-1));
+                q.offer(new Point(x, y+1));
+                
+                
+                q.offer(new Point(x-1, y-1));
+                q.offer(new Point(x-1, y+1));
+                q.offer(new Point(x+1, y-1));
+                q.offer(new Point(x+1, y+1));
             }
+            level++;
         }
-        
         return -1;
     }
 }
