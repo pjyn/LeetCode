b8ff8c0fd1f342d360e7c27061adc7095fda58e5
pjyn <69898451+pjyn@users.noreply.github.com>
2022-07-25 00:08:49 +0530

    Time: 17 ms (75.62%), Space: 42.8 MB (88.84%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.java
+++ 929-unique-email-addresses/929-unique-email-addresses.java
@@ -4,34 +4,10 @@ class Solution {
         Set<String> hs = new HashSet<>();
         
         for(String email: emails){
-            String updatedEmail = get(email);
-            hs.add(updatedEmail);
+            String[] part = email.split("@");
+            String[] l = part[0].split("\\+");
+            hs.add(l[0].replace(".", "") + "@" + part[1]);
         }
         return hs.size();
-    }
-    
-    private String get(String email){
-        String localName = "";
-        
-        int i=0;
-        while(i < email.length() && email.charAt(i) != '@'){
-            if(email.charAt(i) == '.'){
-                i++;
-            }else if(email.charAt(i) == '+'){
-                while(i < email.length() && email.charAt(i) != '@'){
-                    i++;
-                }
-            }else{
-                localName += email.charAt(i);
-                i++;
-            }
-        }
-        i++;
-        String domain = "";
-        while(i < email.length()){
-            domain += email.charAt(i);
-            i++;
-        }
-        return localName + '@' + domain;
     }
 }
