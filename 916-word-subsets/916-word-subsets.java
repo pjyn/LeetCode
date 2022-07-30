fd16f03a50df6b480e71b9b04fdb398707791487
pjyn <69898451+pjyn@users.noreply.github.com>
2022-07-30 23:54:57 +0530

    Time: 41 ms (35.37%), Space: 88 MB (61.38%) - LeetHub

--- 916-word-subsets/916-word-subsets.java
+++ 916-word-subsets/916-word-subsets.java
@@ -3,34 +3,42 @@ class Solution {
         
         int[] maxB = new int[26];
         
-        for(String b: B){
-            int[] f = new int[26];
-            for(char c: b.toCharArray()){
-                f[c-'a']++;
+        for(int i=0; i<B.length; i++){
+            
+            String r = B[i];
+            int[] freq = new int[26];
+            for(char c: r.toCharArray()){
+                freq[c-'a']++;
             }
-            for(int i=0; i<26; i++){
-                maxB[i] = Math.max(maxB[i], f[i]);
+            
+            for(int j=0; j<26; j++){
+                maxB[j] = Math.max(maxB[j], freq[j]);
             }
         }
         
         List<String> res = new ArrayList<>();
         
-        for(String a: A){
-            int[] f = new int[26];
-            for(char c: a.toCharArray()){
-                f[c-'a']++;
+        for(int i=0; i<A.length; i++){
+            
+            String rt = A[i];
+            int[] freq = new int[26];
+            for(char c: rt.toCharArray()){
+                freq[c-'a']++;
             }
-            if(isUniversal(f, maxB)){
-                res.add(a);
+            
+            if(isUniversal(freq, maxB)){
+                res.add(rt);
             }
         }
         return res;
     }
     
-    private boolean isUniversal(int[] f, int[] t){
-        for(int i=0; i<f.length; i++){
-            if(f[i] < t[i])
+    private boolean isUniversal(int[] freq, int[] max){
+        
+        for(int i=0; i<26; i++){
+            if(freq[i] < max[i]){
                 return false;
+            }
         }
         return true;
     }
