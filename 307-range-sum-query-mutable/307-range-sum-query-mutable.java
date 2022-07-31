337c0420946b17220899eaae75ed0422e5bbf0aa
pjyn <69898451+pjyn@users.noreply.github.com>
2022-07-31 18:08:36 +0530

    Time: 196 ms (18.54%), Space: 126.6 MB (72.12%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,44 +1,77 @@
 class NumArray {
-    int[] nums;
-    int[] BIT;
-    int n;
-    
-    public NumArray(int[] nums) {
-        this.nums = nums;
+
+    class SegmentTreeNode{
+        private int start, end;
+        private SegmentTreeNode left, right;
+        private int sum;
         
-        n = nums.length;
-        BIT = new int[n+1];
-        for (int i=0; i<n; i++){
-            init(i, nums[i]);
+        public SegmentTreeNode(int start, int end){
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = 0;
         }
     }
     
-    public void init(int i, int val){
-        i++;
-        while(i <= n){
-            BIT[i] += val;
-            i += (i & -i);
+    SegmentTreeNode root = null;
+    
+    public NumArray(int[] nums) {
+        root = buildTree(nums, 0, nums.length-1);
+    }
+    
+    private SegmentTreeNode buildTree(int[] nums, int start, int end){
+        if(start > end){
+            return null;
+        }else{
+            SegmentTreeNode ret = new SegmentTreeNode(start, end);
+            if(start == end){
+                ret.sum = nums[start];      // leaf nodes
+            }else{
+                int mid = start + (end - start) / 2;
+                ret.left = buildTree(nums, start, mid);
+                ret.right = buildTree(nums, mid+1, end);
+                ret.sum = ret.left.sum + ret.right.sum;
+            }
+            return ret;
         }
     }
     
     public void update(int index, int val) {
-        int diff = val - nums[index];
-        nums[index] = val;
-        init(index, diff);
+        updateHelper(root, index, val);
     }
     
-    public int getSum(int i){
-        int sum = 0;
-        i++;
-        while(i > 0){
-            sum += BIT[i];
-            i -= (i & -i);
+    void updateHelper(SegmentTreeNode root, int pos, int val){
+        if(root.start == root.end) {        // found leaf node to be updated
+            root.sum = val;
+        }else{
+            int mid = root.start + (root.end - root.start) / 2;
+            if(pos <= mid){
+                updateHelper(root.left, pos, val);
+            }else{
+                updateHelper(root.right, pos, val);
+            }
+            root.sum = root.left.sum + root.right.sum;
         }
-        return sum;
     }
     
     public int sumRange(int left, int right) {
-        return getSum(right) - getSum(left-1);
+        return sumRangeHelper(root, left, right);
+    }
+    
+    public int sumRangeHelper(SegmentTreeNode root, int start, int end){
+        if(root.end == end && root.start == start){
+            return root.sum;
+        }else{
+            int mid = root.start + (root.end - root.start) / 2;
+            if(end <= mid){
+                return sumRangeHelper(root.left, start, end);
+            }else if(start >= mid+1){
+                return sumRangeHelper(root.right, start, end);
+            }else{
+                return sumRangeHelper(root.left, start, mid) + sumRangeHelper(root.right, mid+1, end);
+            }
+        }
     }
 }
 
