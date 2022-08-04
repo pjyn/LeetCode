9014fa8e86409b29c5014bff6fb92eaed2bef1f1
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-04 14:43:20 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (76.44%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,26 +1,32 @@
 class Solution {
     public void rotate(int[][] matrix) {
         
-        int row = matrix.length;
-        int col = matrix[0].length;
+        int n = matrix.length;
+        int m = matrix[0].length;
         
-        //transpose
-        for(int i=0; i<row; i++){
-            for(int j=i; j<col; j++){
-                int tmp = matrix[i][j];     // col-val
-                matrix[i][j] = matrix[j][i];    // row-val
+        /*
+            1   4   7
+            2   5   8
+            3   6   9
+            
+            7   4   1
+            8   5   2
+            9   6   3
+        */
+        
+        for(int i=0; i<n; i++){
+            for(int j=i; j<m; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
             }
         }
-        
-        // swap row-wise
-        for(int i=0; i<row; i++){
-            for(int j=0; j<row/2; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m/2; j++){
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][col-1-j];
-                matrix[i][col-1-j] = tmp;
+                matrix[i][j] = matrix[i][m-1-j];
+                matrix[i][m-1-j] = tmp;
             }
         }
-        
     }
 }
