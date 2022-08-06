70e7203fcf5753a69a1415d722c7d9f11940704a
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-06 12:40:33 +0530

    Time: 113 ms (38.10%), Space: 71.4 MB (67.93%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.java
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -1,47 +1,24 @@
 class Solution {
     public int reversePairs(int[] nums) {
-     
+        
+        /*
+            nums[i] > 2 * nums[j]
+        */
         return mergeSort(nums, 0, nums.length-1);
     }
-    
-    int mergeSort(int[] nums, int low, int high){
-        if(low >= high)
+    private int mergeSort(int[] nums, int s, int e){
+        if(s >= e)
             return 0;
-        int mid = (low + high) / 2; 
-        int inv = mergeSort(nums, low, mid);
-        inv += mergeSort(nums, mid+1, high);
-        inv += merge(nums, low, mid, high);
-        return inv;
-    }
-    int merge(int[] nums, int low, int mid, int high){
-        int cnt = 0;
-        int j = mid+1;
-        for(int i=low; i<=mid; i++){
-            while(j <= high && nums[i] > (2*(long) nums[j])) {
-                j++;   
-            }
-            cnt += (j - (mid+1));
-        }
         
-        ArrayList<Integer> tmp = new ArrayList<>();
-        int left = low, right = mid+1;
-        while(left <= mid && right <= high){
-            if(nums[left] <= nums[right]){
-                tmp.add(nums[left++]);
-            }else{
-                tmp.add(nums[right++]);
+        int mid = s + (e-s)/2;
+        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e);
+        for(int i=s, j=mid+1; i<=mid; i++){
+            while(j <= e && nums[i]/2.0 > nums[j]){
+                j++;
             }
+            cnt += j - (mid+1);
         }
-        
-        while(left <= mid){
-            tmp.add(nums[left++]);
-        }
-        while(right <= high){
-            tmp.add(nums[right++]);
-        }
-        for(int i=low; i<=high; i++){
-            nums[i] = tmp.get(i- low);
-        }
+        Arrays.sort(nums, s, e+1);
         return cnt;
     }
 }
