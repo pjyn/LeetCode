134bb4a22a5c8bfbb9923a8ed82811ce9425830d
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-11 14:59:04 +0530

    Time: 3 ms (80.48%), Space: 48.3 MB (21.36%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -12,40 +12,43 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
      
         ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
+        ListNode head_ref = dummy;
+        
         int carry = 0;
         
         while(l1 != null && l2 != null){
-            int sum = l1.val+l2.val+carry;
-            prev.next = new ListNode(sum%10);
-            prev = prev.next;
-            carry = sum/10;
+            int tmp = l1.val + l2.val + carry;
+            
+            
+            dummy.next = new ListNode(tmp%10);
+            dummy = dummy.next;
             l1 = l1.next;
             l2 = l2.next;
+            carry = tmp/10;
         }
         if(l1 != null){
             while(l1 != null){
-                int tmp = l1.val+carry;
-                prev.next = new ListNode(tmp%10);
-                prev = prev.next;
+                int sum = l1.val + carry;
+                dummy.next = new ListNode(sum%10);
+                dummy = dummy.next;
                 l1 = l1.next;
-                carry = tmp/10;
+                carry = sum/10;
             }
         }
         if(l2 != null){
             while(l2 != null){
-                int tmp = l2.val+carry;
-                prev.next = new ListNode(tmp%10);
-                prev = prev.next;
+                int sum = l2.val + carry;
+                dummy.next = new ListNode(sum%10);
+                dummy = dummy.next;
                 l2 = l2.next;
-                carry = tmp/10;
+                carry = sum/10;
             }
         }
         if(carry != 0){
-            prev.next = new ListNode(carry);
-            prev = prev.next;
+            dummy.next = new ListNode(carry);
+            dummy = dummy.next;
+            dummy.next = null;
         }
-        prev.next = null;
-        return dummy.next;
+        return head_ref.next;
     }
 }
