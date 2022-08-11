9a764410ae6091e14f36d455ea819cb9262f8112
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-11 14:07:24 +0530

    Time: 0 ms (100.00%), Space: 43 MB (83.91%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,16 +14,14 @@
  * }
  */
 class Solution {
-    
     public boolean isValidBST(TreeNode root) {
         
-        if(root == null){
+        if(root == null)
             return true;
-        }
-        return rec(root, Long.MIN_VALUE, Long.MAX_VALUE);
         
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    private boolean rec(TreeNode curr, long min, long max){
+    private boolean helper(TreeNode curr, long min, long max){
         if(curr == null){
             return true;
         }
@@ -31,6 +29,7 @@ class Solution {
         if(curr.val >= max || curr.val <= min){
             return false;
         }
-        return (rec(curr.left, min, curr.val) && rec(curr.right, curr.val, max));
+        
+        return (helper(curr.left, min, curr.val) && (helper(curr.right, curr.val, max)));
     }
 }
