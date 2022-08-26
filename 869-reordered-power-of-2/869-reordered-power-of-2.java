179534354fce7009d4ad6caf06c9a167a2fc2844
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-26 15:02:18 +0530

    Time: 2 ms (68.67%), Space: 41 MB (61.33%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+     
+        int[] inputDigitFreq = freqCount(n);
+        
+        for(int i=0; i<31; i++){
+            
+            int powerOf2 = (int)Math.pow(2, i);
+            int[] powerOf2FreqCount = freqCount(powerOf2);
+            if(compareArray(inputDigitFreq, powerOf2FreqCount)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean compareArray(int[] freqCount1, int[] freqCount2){
+        boolean match = true;
+        
+        for(int i=0; i<10; i++){
+            if(freqCount1[i] != freqCount2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private int[] freqCount(int n){
+        int[] digitFreq = new int[10];
+        while(n > 0){
+            digitFreq[n%10]++;
+            n = n/10;
+        }
+        return digitFreq;
+    }
+}
