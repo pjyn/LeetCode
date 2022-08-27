781d658e4f046d1badc7c375d815e01e7f156368
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-27 15:24:22 +0530

    Time: 4 ms (71.23%), Space: 50.3 MB (23.44%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,28 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     
-        ArrayList<Integer> mixed = new ArrayList<>();
-        
-        for(int i: nums1){
-            mixed.add(i);
-        }
-        for(int i: nums2){
-            mixed.add(i);
+        if(nums1.length < nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
         }
         
-        Collections.sort(mixed);
-        double ans = 0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        
+        int low=0, high = n2*2;
+        
+        while(low <= high){
+            int cut2 = (low + high) / 2;
+            int cut1 = n1 + n2 - cut2;
             
-        int size = mixed.size();        
-        
-        if(size%2 == 0){        
-            // 4 / 2 => 2       
-            ans = ( mixed.get(size/2 - 1) + mixed.get(size/2) ) / 2.0;            // 2 + 3
+            double l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[(cut1-1)/2];
+            double l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[(cut2-1)/2];
+            double r1 = (cut1 == n1*2) ? Integer.MAX_VALUE : nums1[(cut1)/2];
+            double r2 = (cut2 == n2*2) ? Integer.MAX_VALUE : nums2[(cut2)/2];
             
-            return ans;
-        }else{
-            ans = ( mixed.get(size/2)  );
-            return ans;
+            if(l1 > r2){
+                low = cut2+1;
+            }else if(l2 > r1){
+                high = cut2-1;
+            }else{
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+            }
         }
+        return -1;
     }
 }
