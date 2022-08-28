2f0ed382857493f4d72c0ec73445c6c1c978463e
pjyn <69898451+pjyn@users.noreply.github.com>
2022-08-28 15:38:41 +0530

    Time: 10 ms (61.20%), Space: 48.4 MB (30.56%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,13 +1,11 @@
 class Solution {
     int[][] res;
-    // boolean[][] vis;
     public int[][] diagonalSort(int[][] mat) {
      
         int row = mat.length;
         int col = mat[0].length;
         
         res = new int[row][col];
-        // vis = new boolean[row][col];
         
         for(int i=0; i<row; i++){
             
@@ -27,23 +25,12 @@ class Solution {
     
     private List<Integer> rec(int[][] mat, int i, int j, List<Integer> curr){
         
-//         if(i < 0 || i > mat.length || j < 0 || j > mat[0].length || vis[i][j]){
-//             Collections.sort(curr);
-//             return curr;
-//         }
-        
-//         vis[i][j] = true;
-        
-        // rec(mat, i+1, j+1, curr);
         while(i < mat.length && j < mat[0].length){
             curr.add(mat[i][j]);
             i++;
             j++;
         }
         Collections.sort(curr);
-        // for(int i: curr){
-        //     System.out.println(i);
-        // }
         return curr;
         
     }
